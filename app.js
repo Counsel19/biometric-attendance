d8a04d3289c56ffc244abcc2d435d14eca39ae91
CounselJs <okpabicounsel@gmail.com>
2023-11-22 10:51:33 +0100

    Fix Build Issues

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
 
-import dotenv from "dotenv";
-dotenv.config();
+// // import dotenv from "dotenv";
+// dotenv.config();
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
